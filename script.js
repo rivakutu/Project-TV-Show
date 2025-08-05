7d7f65da18058d594848abb6229b8569625b6275
rivakutu <revivemmapfumo@gmail.com>
2025-08-05 16:02:55 +0200

    Displays episode information on the page
    Implements the display of episode information including
    episode code, title, image, and summary.
    
    It also adds a link to TVMaze as a data source.

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
-//You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
+// formatting episode and season codes for each episode
+function formatEpisodeCode(season, episode) {
+  const seasonStr = season.toString().padStart(2, "0");
+  const episodeStr = episode.toString().padStart(2, "0");
+  return `S${seasonStr}E${episodeStr}`;
+}
+// function to create HTML element for each episode
+function createEpisodeElement(episode) {
+  const episodeDiv = document.createElement("div");
+  episodeDiv.className = "episode-card";
 
+  const episodeCode = formatEpisodeCode(episode.season, episode.number);
+
+  //creating title element
+  const title = document.createElement("h3");
+  title.textContent = `${episodeCode} - ${episode.name}`;
+
+  //creating image element
+  const image = document.createElement("img");
+  image.src = episode.image.medium;
+  image.alt = episode.name;
+
+  //creating summary element
+  const summary = document.createElement("div");
+  summary.className = "episode-summary";
+  summary.innerHTML = episode.summary;
+
+  // we need to append all elements to the episodeDiv
+  episodeDiv.appendChild(title);
+  episodeDiv.appendChild(image);
+  episodeDiv.appendChild(summary);
+
+  return episodeDiv;
+}
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // clearing existing content
+  rootElem.innerHTML = "";
+
+  //we need to loop through all episodes and create an HTML element for each episode
+  episodeList.forEach((episode) => {
+    const episodeElement = createEpisodeElement(episode);
+    rootElem.appendChild(episodeElement);
+  });
+
+  //referencing TVMaze
+  const tribute = document.createElement("footer");
+  tribute.innerHTML =
+    '<p>Data came from <a href="https://www.tvmaze.com/">TVMaze</a></p>';
+  rootElem.appendChild(tribute);
 }
 
 window.onload = setup;
